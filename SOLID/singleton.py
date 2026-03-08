71245a3f3c8be12c71c49ad5624ac5e0baa8a475
Mainakdey1 <mainakde02@gmail.com>
2026-03-08 21:59:04 +0530

    Initial Commit

---
+++ SOLID/singleton.py
@@ -0,0 +1,12 @@
+class InvoiceGenerator:
+    def generate(self):
+        print('generating report')
+        pass
+
+class ReportSaver:
+    def save_to_file(self):
+        print('Saving report')
+        pass
+#this pattern ensures that a class only has one responsibility. Here two classes 
+#are handling separate responsibilities
+
